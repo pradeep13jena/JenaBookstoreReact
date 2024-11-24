c82235a4378f53d247f08ffe9eece95a4ae7307c
pradeep13jena <goldrushatjenas@gmail.com>
2024-11-24 12:37:37 +0530

    heh

--- src/components/BookmarkCard.jsx
+++ src/components/BookmarkCard.jsx
@@ -1,5 +1,4 @@
 import React, {useContext} from "react";
-import { Link } from "react-router-dom";
 import booksContext from '../utils/booksContext.js'
 
 export default function BookmarkCard(Props) {
